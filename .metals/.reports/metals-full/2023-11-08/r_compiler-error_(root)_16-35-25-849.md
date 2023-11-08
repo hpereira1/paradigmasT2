cf3bf728540c289d0730a042f09d899482e1ade5
hpereira1 <pereirah123@gmail.com>
2023-11-08 18:05:50 -0300

    checkOrthogonallyAdjacent working

---
+++ .metals/.reports/metals-full/2023-11-08/r_compiler-error_(root)_16-35-25-849.md
@@ -0,0 +1,28 @@
+file://<WORKSPACE>/kojun/src/main/scala/Main.scala
+### java.lang.OutOfMemoryError: Java heap space
+
+occurred in the presentation compiler.
+
+action parameters:
+offset: 66
+uri: file://<WORKSPACE>/kojun/src/main/scala/Main.scala
+text:
+```scala
+@main def hello: Unit =
+  println("Hello world!")
+  println(msg)
+o@@
+def msg = "I was compiled by Scala 3. :)"
+
+```
+
+
+
+#### Error stacktrace:
+
+```
+
+```
+#### Short summary: 
+
+java.lang.OutOfMemoryError: Java heap space
--- kojun/src/main/scala/Main.scala
+++ kojun/src/main/scala/Main.scala
@@ -1,5 +1,23 @@
 object Kojun {
   
+  type Board = Array[Array[Int]]
+  
+
+
+  def checkOrthogonallyAdjacent(num : Int, board : Board, row : Int, col : Int): Boolean = {
+    val lenBoard = board.length
+    val notUp = if (row - 1 >= 0) board(row - 1)(col) != num else true
+
+    val notDown = if (row + 1 < lenBoard ) board(row + 1)(col) != num else true
+
+    val notLeft = if (col - 1 >= 0) board(row)(col - 1) != num else true
+
+    val notRight = if (col + 1 < lenBoard) board(row)(col + 1) != num else true 
+
+    notUp && notDown && notLeft && notRight
+  }
+
+
   def main(args:Array[String]): Unit = {
     val problem = 
       Array(
@@ -23,5 +41,14 @@ object Kojun {
         Array(12,12,12,12,17,14,14,14),
         Array(16,17,17,17,17,14,18,18),
       )
+      
+      val x =checkOrthogonallyAdjacent(3, problem, 0, 2) // false notDown == false
+      val x1 =checkOrthogonallyAdjacent(3, problem, 1, 3) // false notLeft == false
+      val x2 =checkOrthogonallyAdjacent(3, problem, 3, 1) // false notRight == false
+      val x3 =checkOrthogonallyAdjacent(3, problem, 3, 5) // false notUp == false
+      println(x)
+      println(x1)
+      println(x2)
+      println(x3)
   }
 }
